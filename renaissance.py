ff2969dd98a812af582ab475c2923bb787569acd
Balazs Tobak <bobaktalazs@gmail.com>
2026-04-19 17:15:08 +0200

    eredeti fajlok commitolasa

---
+++ renaissance.py
@@ -0,0 +1,22 @@
+from artifact import Painting, Sculpture, Building
+
+renaissance_painting = Painting(
+    "Mona Lisa",
+    1503,
+    "Leonardo da Vinci",
+    "oil"
+)
+
+renaissance_sculpture = Sculpture(
+    "David",
+    1504,
+    "marble",
+    517
+)
+
+renaissance_building = Building(
+    "St. Peter's Basilica",
+    1506,
+    "Donato Bramante",
+    "Vatican City"
+)
